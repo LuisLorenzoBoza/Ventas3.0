bb62fbd49cb459824592e585473c01c44a8ef5d2
luis lorenzo <sixsixonelui@gmail.com>
2019-01-28 10:11:29 -0400

    Dandole forma

--- Ventas3.0/Global.asax.cs
+++ Ventas3.0/Global.asax.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace Ventas3._0
 {
@@ -14,6 +11,7 @@ namespace Ventas3._0
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
