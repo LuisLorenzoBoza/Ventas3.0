b7367a00811d0d59a2662b9604ce25cfc89ba1f3
luis lorenzo <sixsixonelui@gmail.com>
2019-01-28 00:07:36 -0400

    Mis disculpas por no hacer comit al crear el proyecto , pero es que es el segundo que hago!

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using Entities;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
---
+++ Entities/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioID { get; set; }
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+        public string NombreUsuario { get; set; }
+        public DateTime Fecha { get; set; }
+        
+
+        public Usuario(int UsuarioID, string Contraseña, string Nombre, DateTime Fecha)
+        {
+            this.UsuarioID = UsuarioID;
+            this.Clave = Contraseña;
+            this.Nombre = Nombre;
+            this.Fecha = Fecha;
+            this.CuentaUsua = CuentaUsua;
+        }
+
+        public Usuario()
+        {
+            this.UsuarioID = 0;
+            this.Clave = string.Empty;
+            this.Nombre = string.Empty;
+            this.Fecha = DateTime.Now;
+            CuentaUsua = string.Empty;
+        }
+    }
+}
